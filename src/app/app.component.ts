a747cf0ac680f37184f12325567b8e71524a828e
Sultan Kuliev <180474@edu.psuti.ru>
2020-05-28 16:37:24 +0400

    refactor: transferring app a to angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,176 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {fromFetch} from 'rxjs/fetch';
+import {map, switchMap} from 'rxjs/operators';
+import {
+  combineLatest, of, throwError, from, Observable
+} from 'rxjs';
+import load from 'ymaps-loader';
+
+interface WeatherData {
+  first: any;
+  rest: any[];
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'fancy-weather-angular';
+  myMap: any;
+  api = 'd7f53ede-df5f-4048-b180-a767f8642bcc';
+  weatherData: Observable<WeatherData>;
+  coords;
+  date: string;
+  test = false;
+
+  // weather
+  constructor() {
+    this.getUserLocation()
+      .then((data) => {
+        const coords = data.loc.split(',');
+        this.mapInit(coords[0], coords[1]);
+      });
+  }
+
+  ngOnInit() {
+    setInterval(() => {
+      const dateNow = new Date();
+      const options = {
+        weekday: 'long',
+        year: 'numeric',
+        month: 'long',
+        day: 'numeric',
+      };
+      this.date = `${dateNow.toLocaleString('ru', options)} ${dateNow.toLocaleTimeString()}`;
+    }, 1000);
+  }
+
+  getWeekDay(data) {
+    let date = data.forecast.forecastday[0].date.split('-');
+    date = new Date(date[0], date[1] - 1, date[2], 0, 0, 0);
+    const daysOfWeek = ['Воскресенье', 'Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота'];
+
+    return daysOfWeek[date.getDay()];
+  }
+
+  setWeather(city) {
+    // function setTextContent(id, value) {
+    //   document.getElementById(id).textContent = value;
+    // }
+
+    // function setCurrentDay(data) {
+    //   setTextContent('title', `${data.location.name}, ${data.location.country}`);
+    //
+    //   setTextContent('current-value', Math.trunc(data.current.temp_c));
+    //   setTextContent('condition', data.current.condition.text);
+    //   setTextContent('feel', `Feels like: ${data.current.feelslike_c}`);
+    //   setTextContent('wind', `Wind: ${data.current.wind_kph}`);
+    //   setTextContent('humidity', `Humidity: ${data.current.humidity}`);
+    // }
+
+    function getFormatDate(date) {
+      return date < 10 ? `0${date}` : date;
+    }
+
+    // function setNextThree(data) {
+    //   data.forEach(({forecast}, i) => {
+    //     const day = i + 1;
+    //     // setTextContent(`day-week-${day}`, getWeekDay(date.getDay()));
+    //     setTextContent(`day-value-${day}`, Math.trunc(forecast.forecastday[0].day.avgtemp_c));
+    //     // setTextContent(`day-icon-${day}`, );
+    //   });
+    // }
+
+    const apiKey = '5c1b1eabad7f4b35af8144627202405';
+
+    function makeReq(i) {
+      const date = new Date();
+      date.setDate(date.getDate() + i);
+      const dateParse = `${date.getFullYear()}-${getFormatDate(date.getMonth() + 1)}-${getFormatDate(date.getDate())}`;
+      return fromFetch(
+        `https://api.weatherapi.com/v1/forecast.json?key=${apiKey}&q=${city}&dt=${dateParse}`,
+      );
+    }
+
+    // console.log([0, 0, 0, 0].map((v, i) => makeReq(i)));
+    this.weatherData = combineLatest([0, 0, 0, 0].map((v, i) => makeReq(i))).pipe(
+      switchMap((responses) => combineLatest(
+        responses.map((rsp) => from(
+          rsp.ok
+            ? rsp.json()
+            : (rsp.status === 400
+              ? of(null)
+              : throwError(`${rsp.status}`)
+            ),
+        )),
+      )),
+      map(([first, ...rest]: [any, any[]]) => ({first, rest}))
+    );
+  }
+
+  setPositionOnMap(coords, lower, upper) {
+    const latitude = coords[1];
+    const longitude = coords[0];
+    this.coords = {
+      latitude: {
+        int: Math.trunc(latitude),
+        frac: Math.trunc(latitude % 1 * 100),
+      },
+      longitude: {
+        int: Math.trunc(longitude),
+        frac: Math.trunc(longitude % 1 * 100),
+      }
+    };
+    this.myMap.setBounds([lower.reverse(), upper.reverse()]);
+  }
+
+  getLocation(city) {
+    const url = `https://geocode-maps.yandex.ru/1.x/?apikey=${this.api}&format=json&geocode=${city}`;
+    return fetch(url)
+      .then((res) => res.json());
+  }
+
+  getUserLocation() {
+    const apiKey = 'eb5b90bb77d46a';
+    return fetch(`https://ipinfo.io/json?token=${apiKey}`)
+      .then((res) => res.json());
+  }
+
+  mapInit(latitude, longitude) {
+    load({apiKey: this.api})
+      .then((maps) => {
+        this.myMap = new maps.Map('map', {
+          center: [latitude, longitude],
+          zoom: 12,
+          controls: [],
+        });
+      });
+  }
+
+  setBackground = () => {
+    const body = document.querySelector('body');
+    return () => {
+      const apiKey = 'h4mP-4wa51P8cSyCYVfzNhWbqskv0MtF-IOu1Mj9_Cg';
+      const orientation = 'landscape';
+      const url = `https://api.unsplash.com/photos/random?orientation=${orientation}&query=weather&client_id=${apiKey}`;
+      fetch(url)
+        .then((res) => res.json())
+        .then((photo) => {
+          body.style.background = `linear-gradient(rgba(8, 15, 26, 0.59) 0%, rgba(17, 17, 46, 0.46) 100%), url('${photo.urls.full}') center center / cover fixed`;
+        });
+    };
+  }
+
+  searchButtonClick(query: string) {
+    this.setWeather(query);
+    this.getLocation(query)
+      .then((data) => {
+        const coords = data.response.GeoObjectCollection.featureMember[0].GeoObject.Point.pos.split(' ');
+        const lower = data.response.GeoObjectCollection.featureMember[0].GeoObject.boundedBy.Envelope.lowerCorner.split(' ');
+        const upper = data.response.GeoObjectCollection.featureMember[0].GeoObject.boundedBy.Envelope.upperCorner.split(' ');
+        this.setPositionOnMap(coords, lower, upper);
+      });
+  }
 }
